a66589c21f7fe1788b20a5c79d2dff7673e247e4
elliottcable <git@elliottcable.name>
2008-12-23 15:10:45 -0900

    Added a paths list to the Launchd class

--- lib/launchdr/launchd.rb
+++ lib/launchdr/launchd.rb
@@ -3,6 +3,14 @@ require 'facets/string'
 module LaunchDoctor
   
   class Launchd < PropertyList
+    Paths = {
+      :user_agent => "~/Library/LaunchAgents",
+      :agent => "/Library/LaunchAgents",
+      :daemon => "/Library/LaunchDaemons", 
+      :system_agent => "/System/Library/LaunchAgents",
+      :system_daemon => "/System/Library/LaunchDaemons"
+    }
+    
     # Nothing here as of yet. Maybe some convenience methods to come.
     
     def [] key
