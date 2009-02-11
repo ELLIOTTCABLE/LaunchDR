4108c7db57f3e0d5a1c100d7e387add55a834ca8
elliottcable <git@elliottcable.name>
2009-02-11 03:22:55 -0900

    Added a bit more logic to the rake task to force it to check for the existance of the binary

--- lib/launchdr/task.rb
+++ lib/launchdr/task.rb
@@ -9,12 +9,22 @@ module LaunchDr
 
       desc opts[:desc] unless opts[:no_desc]
       task name do
+        path = File.expand_path File.join('', opts[:bin])
+        unless File.file? path
+          path = File.expand_path File.join(Gem::default_bindir, opts[:bin])
+          unless File.file? path
+            path = File.expand_path %x[which "#{opts[:bin]}"]
+          end
+        end
+        unless File.file? path
+          raise "** Unable to locate binary #{opts[:bin]}"
+        end
         LaunchDr "rb.launchdr.#{opts[:bin]}" do |plist|
-          plist[:program_arguments] = [File.join('/usr/local/bin', opts[:bin]), opts[:arguments]].flatten
+          plist[:program_arguments] = [path, opts[:arguments]].flatten
           plist[:keep_alive] = true
           plist[:run_at_load] = true
         end
-        puts "** `#{[opts[:bin], opts[:arguments]].flatten.join(' ')}` will now be run on startup!"
+        puts "** `#{[path, opts[:arguments]].flatten.join(' ')}` will now be run on startup!"
       end
 
     end
